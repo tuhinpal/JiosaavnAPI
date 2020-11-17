29d3eacdc4d53391d939792da5ec5304ba0f4e07
Tuhin Kanti Pal <me@mailtuhin.ml>
2020-11-17 09:23:47 +0530

    Improved

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//Made by Tuhin
-//Visit https://tu.hin.life
-//Contact me@thetuhin.com
-
 const express = require('express');
 const request = require('request');
 const app = express()
@@ -12,6 +8,13 @@ const songbase = "https://www.jiosaavn.com/api.php?__call=song.getDetails&cc=in&
 const lyricsbase = "https://www.jiosaavn.com/api.php?__call=lyrics.getLyrics&ctx=web6dot0&api_version=4&_format=json&_marker=0%3F_marker%3D0&lyrics_id=";
 const albumbase = "https://www.jiosaavn.com/api.php?__call=content.getAlbumDetails&_format=json&cc=in&_marker=0%3F_marker%3D0&albumid="
 
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Cache-Control': 'no-cache, no-store, must-revalidate',
+    'Content-Type': 'application/json; charset=utf-8',
+    'Open-Source': 'https://github.com/cachecleanerjeet/jiosaavnapi'
+}
+
 app.get('/', (req, res) => {
     res.send(`Server is Running<br><br>Api Documentation <a href="https://github.com/cachecleanerjeet/jiosaavnapi#readme">Here</a>.<br><br>An Open Source Project by <a href="https://tu.hin.life">Tuhin</a>.`)
 })
@@ -19,6 +22,8 @@ app.get('/', (req, res) => {
 // search a song
 app.get('/search', (req, res) => {
     let query = req.query.query;
+    res.status(200)
+    res.header(headers)
     var options = {
         'method': 'GET',
         'url': (searchbase) + (query)
@@ -42,16 +47,13 @@ app.get('/search', (req, res) => {
             }
         }
     });
-    res.status(200);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Content-Type', 'application/json');
-    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.header('Open-Source', 'https://github.com/cachecleanerjeet/jiosaavnapi');
 })
 
 // fetch detail of a song
 app.get('/song', (req, res) => {
     let id = req.query.id;
+    res.status(200)
+    res.header(headers)
     var options = {
         'method': 'GET',
         'url': (songbase) + (id)
@@ -78,17 +80,14 @@ app.get('/song', (req, res) => {
                 res.send(output);
             }
         }
-    });
-    res.status(200);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Content-Type', 'application/json');
-    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.header('Open-Source', 'https://github.com/cachecleanerjeet/jiosaavnapi');
+    })
 })
 
 // Fetch lyrics of a song
 app.get('/lyrics', (req, res) => {
-    let id = req.query.id;
+    let id = req.query.id
+    res.status(200)
+    res.header(headers)
     var options = {
         'method': 'GET',
         'url': (lyricsbase) + (id)
@@ -109,17 +108,14 @@ app.get('/lyrics', (req, res) => {
                 res.send(format);
             }
         }
-    });
-    res.status(200);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Content-Type', 'application/json');
-    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.header('Open-Source', 'https://github.com/cachecleanerjeet/jiosaavnapi');
+    })
 })
 
 // search an album
 app.get('/albumsearch', (req, res) => {
-    let query = req.query.query;
+    let query = req.query.query
+    res.status(200)
+    res.header(headers)
     var options = {
         'method': 'GET',
         'url': (searchbase) + (query)
@@ -142,17 +138,14 @@ app.get('/albumsearch', (req, res) => {
                 res.send(output);
             }
         }
-    });
-    res.status(200);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Content-Type', 'application/json');
-    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.header('Open-Source', 'https://github.com/cachecleanerjeet/jiosaavnapi');
+    })
 })
 
 // fetch an album
 app.get('/album', (req, res) => {
-    let id = req.query.id;
+    let id = req.query.id
+    res.status(200)
+    res.header(headers)
     var options = {
         'method': 'GET',
         'url': (albumbase) + (id)
@@ -177,18 +170,14 @@ app.get('/album', (req, res) => {
                 res.send(output);
             }
         }
-    });
-    res.status(200);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Content-Type', 'application/json');
-    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.header('Open-Source', 'https://github.com/cachecleanerjeet/jiosaavnapi');
+    })
 })
 
 // getting song from Jiosaavn link
 app.get('/link', (req, res) => {
-    let query = req.query.query;
-
+    let query = req.query.query
+    res.status(200)
+    res.header(headers)
     var getsid = {
         'method': 'GET',
         'url': query
@@ -226,14 +215,10 @@ app.get('/link', (req, res) => {
                 })
             }
         }
-    });
-    res.status(200);
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Content-Type', 'application/json');
-    res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.header('Open-Source', 'https://github.com/cachecleanerjeet/jiosaavnapi');
+    })
 })
 
-app.listen(process.env.PORT || 8080, () => {
-    console.log('Listening')
+var PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+    console.log('Listening in Port ' + PORT)
 })
